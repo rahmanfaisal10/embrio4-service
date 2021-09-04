248f551e327d4f2629cc27b6704877bf58d3de89
rahman.faisal100695 <rahman.faisal100695@bluebirdgroup.com>
2021-09-04 18:13:16 +0700

    commit

--- pkg/repository/dashboard.go
+++ pkg/repository/dashboard.go
@@ -179,48 +179,73 @@ func (repo *repository) InsertDashboard() error {
 func (repo *repository) ViewDashboard(mantri string) (*response.ViewDashboard, error) {
 	response := new(response.ViewDashboard)
 	query := `SELECT
-			SUM(COALESCE(ts.total_os,0)) as target_os,
-			SUM(d.os_total) as pencapaian_os,
-			SUM(COALESCE(((ts.total_os * 116/100)-d.os_total)/(12-MONTH(CURDATE())+1),0)) as minimal_delta_nilai_4,
-			SUM((case when u.next_pmt_date >= u.Periode then u.Plafond / CAST(TRIM(BOTH 'M' FROM u.jangka_waktu) as SIGNED) else 0 end)) as angsuran_pokok_belum_masuk,
-			SUM(d.pencapaian_realisasi) as pencapaian_realisasi,
-			SUM(d.sisa_suplesi) as sisa_suplesi,
-			SUM(d.rincian_lunas_hutang) as rincian_lunas_hutang,
-			SUM(COALESCE(d.dh_total,0)) as dh_total,
-			SUM(d.dpk_total) as dpk_posisi,
-			(select count(d.dpk_total) from dashboard d WHERE d.dpk_total != 0) as count_dpk_posisi,
-			SUM(d.dpk_baru) as dpk_baru,
-			(select count(d.dpk_baru) from dashboard d WHERE d.dpk_baru != 0) as count_dpk_baru,
-			SUM(d.dpk_1) as dpk_1,
-			(select count(d.dpk_1) from dashboard d WHERE d.dpk_1 != 0) as count_dpk_1,
-			SUM(d.dpk_2) as dpk_2,
-			(select count(d.dpk_2) from dashboard d WHERE d.dpk_2 != 0) as count_dpk_2,
-			SUM(d.dpk_3) as dpk_3,
-			(select count(d.dpk_3) from dashboard d WHERE d.dpk_3 != 0) as count_dpk_3,
-			SUM(d.dpk_3) as dpk_calon_npl,
-			(select count(d.dpk_3) from dashboard d WHERE d.dpk_3 != 0) as count_dpk_calon_npl,
-			SUM(d.dpk_blm_restruk) as dpk_blm_restruk,
-			(select count(d.dpk_blm_restruk) from dashboard d WHERE d.dpk_blm_restruk != 0) as count_dpk_blm_restruk,
-			SUM(d.lancar_blm_jth_tempo) as lancar_blm_jatuh_tempo,
-			(select count(d.lancar_blm_jth_tempo) from dashboard d WHERE d.lancar_blm_jth_tempo != 0) as count_lancar_blm_jatuh_tempo,
-			SUM(d.npl_total) as npl_total,
-			(select count(d.npl_total) from dashboard d WHERE d.npl_total != 0) as count_npl_total,
-			SUM(d.npl_kl_kecil) as npl_Kl_baru,
-			(select count(d.npl_kl_kecil) from dashboard d WHERE d.npl_kl_kecil != 0) as count_npl_kl_baru,
-			SUM(d.npl_kl_total) as npl_Kl,
-			(select count(d.npl_kl_total) from dashboard d WHERE d.npl_kl_total != 0) as count_npl_kl,
-			SUM(d.npl_diragukan_total) as npl_diragukan_total,
-			(select count(d.npl_diragukan_total) from dashboard d WHERE d.npl_diragukan_total != 0) as count_npl_diragukan_total,
-			SUM(d.npl_macet) as npl_macet,
-			(select count(d.npl_macet) from dashboard d WHERE d.npl_macet != 0) as count_npl_macet,
-			SUM(d.npl_belum_restruk) as npl_belum_restruk,
-			(select count(d.npl_belum_restruk) from dashboard d WHERE d.npl_belum_restruk != 0) as count_npl_belum_restruk
-		FROM dashboard d
-		join target_smk ts on ts.pn = d.Id_mantri
-		join upload u on d.Id_mantri = u.pn_pengelola
-		WHERE d.Id_mantri = ?;`
+					COALESCE(ts.total_os,0) as target_os,
+					d3.pencapaian_os,
+					COALESCE(((ts.total_os * 116/100)-d3.pencapaian_os)/(12-MONTH(CURDATE())+1),0) as minimal_delta_nilai_4,
+					(case when u.next_pmt_date >= u.Periode then u.Plafond / CAST(TRIM(BOTH 'M' FROM u.jangka_waktu) as SIGNED) else 0 end) as angsuran_pokok_belum_masuk,
+					d3.pencapaian_realisasi as pencapaian_realisasi,
+					d3.sisa_suplesi as sisa_suplesi,
+					d3.rincian_lunas_hutang as rincian_lunas_hutang,
+					COALESCE(d3.dh_total,0) as dh_total,
+					d3.dpk_posisi as dpk_posisi,
+					d3.count_dpk_posisi,
+					d3.dpk_baru,
+					d3.count_dpk_baru,
+					d3.dpk_1,
+					(select count(d.dpk_1) from dashboard d WHERE d.dpk_1 != 0 and d.Id_mantri = ? AND d.periode = (SELECT max(m.periode) from dashboard m)) as count_dpk_1,
+					d3.dpk_2,
+					(select count(d.dpk_2) from dashboard d WHERE d.dpk_2 != 0 and d.Id_mantri = ? AND d.periode = (SELECT max(m.periode) from dashboard m)) as count_dpk_2,
+					d3.dpk_3,
+					(select count(d.dpk_3) from dashboard d WHERE d.dpk_3 != 0 and d.Id_mantri = ? AND d.periode = (SELECT max(m.periode) from dashboard m)) as count_dpk_3,
+					d3.dpk_calon_npl,
+					(select count(d.dpk_3) from dashboard d WHERE d.dpk_3 != 0 and d.Id_mantri = ? AND d.periode = (SELECT max(m.periode) from dashboard m)) as count_dpk_calon_npl,
+					d3.dpk_blm_restruk,
+					(select count(d.dpk_blm_restruk) from dashboard d WHERE d.dpk_blm_restruk != 0 and d.Id_mantri = ? AND d.periode = (SELECT max(m.periode) from dashboard m)) as count_dpk_blm_restruk,
+					d3.lancar_blm_jatuh_tempo,
+					(select count(d.lancar_blm_jth_tempo) from dashboard d WHERE d.lancar_blm_jth_tempo != 0 and d.Id_mantri = ? AND d.periode = (SELECT max(m.periode) from dashboard m)) as count_lancar_blm_jatuh_tempo,
+					d3.npl_total,
+					(select count(d.npl_total) from dashboard d WHERE d.npl_total != 0 and d.Id_mantri = ? AND d.periode = (SELECT max(m.periode) from dashboard m)) as count_npl_total,
+					d3.npl_Kl_baru,
+					(select count(d.npl_kl_kecil) from dashboard d WHERE d.npl_kl_kecil != 0 and d.Id_mantri = ? AND d.periode = (SELECT max(m.periode) from dashboard m)) as count_npl_kl_baru,
+					d3.npl_Kl,
+					(select count(d.npl_kl_total) from dashboard d WHERE d.npl_kl_total != 0 and d.Id_mantri = ? AND d.periode = (SELECT max(m.periode) from dashboard m)) as count_npl_kl,
+					d3.npl_diragukan_total,
+					(select count(d.npl_diragukan_total) from dashboard d WHERE d.npl_diragukan_total != 0 and d.Id_mantri = ? AND d.periode = (SELECT max(m.periode) from dashboard m)) as count_npl_diragukan_total,
+					d3.npl_macet,
+					(select count(d.npl_macet) from dashboard d WHERE d.npl_macet != 0 and d.Id_mantri = ? AND d.periode = (SELECT max(m.periode) from dashboard m)) as count_npl_macet,
+					d3.npl_belum_restruk,
+					(select count(d.npl_belum_restruk) from dashboard d WHERE d.npl_belum_restruk != 0 and d.Id_mantri = ? AND d.periode = (SELECT max(m.periode) from dashboard m)) as count_npl_belum_restruk
+					FROM dashboard d
+				join target_smk ts on ts.pn = d.Id_mantri
+				join upload u on d.Id_mantri = u.pn_pengelola
+				join (SELECT 
+					d2.Id_mantri, 
+					SUM(d2.os_total) as pencapaian_os, 
+					SUM(d2.pencapaian_realisasi) as pencapaian_realisasi,
+					SUM(d2.sisa_suplesi) as sisa_suplesi,
+					SUM(d2.rincian_lunas_hutang) as rincian_lunas_hutang,
+					SUM(COALESCE(d2.dh_total,0)) as dh_total,
+					SUM(d2.dpk_total) as dpk_posisi,
+					(select count(d.dpk_total) from dashboard d WHERE d.dpk_total != 0 and d.Id_mantri = ? AND d.periode = (SELECT max(m.periode) from dashboard m)) as count_dpk_posisi,
+					SUM(d2.dpk_baru) as dpk_baru,
+					(select count(d.dpk_baru) from dashboard d WHERE d.dpk_baru != 0 and d.Id_mantri = ? AND d.periode = (SELECT max(m.periode) from dashboard m)) as count_dpk_baru,
+					SUM(d2.dpk_1) as dpk_1,
+					SUM(d2.dpk_2) as dpk_2,
+					SUM(d2.dpk_3) as dpk_3,
+					SUM(d2.dpk_3) as dpk_calon_npl,
+					SUM(d2.dpk_blm_restruk) as dpk_blm_restruk,
+					SUM(d2.lancar_blm_jth_tempo) as lancar_blm_jatuh_tempo,
+					SUM(d2.npl_total) as npl_total,
+					SUM(d2.npl_kl_kecil) as npl_Kl_baru,
+					SUM(d2.npl_kl_total) as npl_Kl,
+					SUM(d2.npl_diragukan_total) as npl_diragukan_total,
+					SUM(d2.npl_macet) as npl_macet,
+					SUM(d2.npl_belum_restruk) as npl_belum_restruk
+				from dashboard d2 group by d2.Id_mantri, d2.periode) d3 on d3.Id_mantri = d.Id_mantri 
+				WHERE d.Id_mantri = ? AND
+				d.periode = (SELECT max(m.periode) from dashboard m);`
 
-	err := repo.db.Get(response, query, mantri)
+	err := repo.db.Get(response, query, mantri, mantri, mantri, mantri, mantri, mantri, mantri, mantri, mantri, mantri, mantri, mantri, mantri, mantri, mantri)
 	if err != nil {
 		log.Error(err)
 		return nil, err
